a1e95a7606910071b4d46be0596b3c8ea75b94ec
Nicolas Palopoli <nicopalo@gmail.com>
2016-03-03 19:38:05 -0300

    [DONE] Included ELMotiSt_PDBssSEQATOM.py to generate parsed table of ELM structural information including 2nd structure and SEQATOM/SEQRES mapping.

---
+++ ELMotiSt_PDBssSEQATOM.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+  Generate parsed table of ELM structural information with 2nd structure and SEQATOM/SEQRES mapping
+  File name: ELMotiSt_PDBssSEQATOM.py
+  Author: Nicolas Palopoli
+  Date created: 2015/10/05
+  Date last modified: 2016/03/03
+  Python Version: 2.7
+'''
+
+import sys
+from collections import OrderedDict
+import csv
+from Bio import SeqIO
+
+# Read input files
+try:
+  infasta = open(sys.argv[1])
+  injnet = open(sys.argv[2])
+  inELMinstances = open(sys.argv[3])
+  inSIFTSparse = sys.argv[4]
+  primaryAcc = sys.argv[5]
+except IndexError:
+  print("Input file(s) not specified. Format: ./ELMotiSt_PDBssSEQATOM.py <in.fasta> <in.jnet> <elm_instances[.date].tsv> <parseSIFTS.out> <primaryAcc>")
+  exit()
+except IOError:
+  print("Input file(s) not found. Format: ./ELMotiSt_PDBssSEQATOM.py <in.fasta> <in.jnet> <elm_instances[.date].tsv> <parseSIFTS.out> <primaryAcc>")
+  exit()
+
+def readFasta(infasta):
+  '''Store fasta sequences from file.'''
+  seqs = OrderedDict()
+#  seqs={}  # dict for raw seqs
+  readFirstSeq = False
+  for line in infasta:
+    if line.strip() or line not in ['\n', '\r\n']:  # avoid empty or only whitespace lines
+      line=line.rstrip()  # discard newline at the end (if any)
+      if line[0]=='>':  # or line.startswith('>'); distinguish header
+        if readFirstSeq:  # exit if more than 2 sequences
+          break
+        readFirstSeq = True
+        words=line.split()
+        name=words[0][1:]
+        seqs['res']=''
+      else :  # sequence, not header, possibly multi-line
+        seqs['res'] = seqs['res'] + line
+  seqs['res'] = list(seqs['res'])
+  seqs['position'] = range(1,len(seqs['res'])+1)
+  seqs['name'] = [name] * len(seqs['res'])
+  return seqs
+
+def readJPred(injnet):
+  '''Store JPred predictions by program from file.'''
+#  entries={}  # dict for raw entries
+  entries = OrderedDict()
+  for line in injnet:
+    if line.strip() or line not in ['\n', '\r\n']:  # avoid empty or only whitespace lines
+      line = line.rstrip()  # discard newline at the end (if any)
+      words = line.split(':')
+      program = words[0]  # programs are keys
+      entries[program] = ''
+      values = words[1].split(',')  # predictions are values
+      entries[program] = values[0:-1]
+  tempValue = []
+  for value in entries['JNETJURY']:  # replace empty values with dashes
+    if value == '*':
+      tempValue.append('*')
+    else:
+      tempValue.append('-')
+  entries['JNETJURY'] = tempValue
+  return entries
+
+def readELMinstances(infile):
+  '''Store ELM instances information as list of dicts'''
+  elm = csv.DictReader(filter(lambda row: row[0]!='#', infile),delimiter='\t', quotechar='"')
+  return elm
+
+def mapELMpositions(parsedELM,primaryAcc):
+  '''Make dict with [ELMAccession:[Start,End]]'''
+  ELMpos = {}
+  for row in parsedELM:
+    if primaryAcc == row['Primary_Acc']:
+#      ELMpos[row['Accession']] = [row['Start'],row['End']]
+      ELMpos[row['Accession']] = [row['Start'],row['End'],row['ELMType'],row['ELMIdentifier']]
+#      ELMpos[row['Accession']] = (row['Start'],row['End'])
+  return ELMpos
+
+def placeELM(seq,ELMpos):
+  '''Map ELM to fasta sequence'''
+  seq['ELMpos'] = list('-' * len(seq['res']))
+  seq['ELMacc'] = list('-' * len(seq['res']))
+  seq['ELMType'] = list('-' * len(seq['res']))
+  seq['ELMIdentifier'] = list('-' * len(seq['res']))
+#  for accession, limits in ELMpos.iteritems():
+#    for pos in range(int(limits[0])-1,int(limits[1])):
+  for accession, vals in ELMpos.iteritems():
+    for pos in range(int(vals[0])-1,int(vals[1])):
+      seq['ELMpos'][pos] = seq['res'][pos]
+      if '-' in seq['ELMacc'][pos]: 
+        seq['ELMacc'][pos] = accession
+        seq['ELMType'][pos] = vals[2]
+        seq['ELMIdentifier'][pos] = vals[3]
+      else:
+        seq['ELMacc'][pos] = seq['ELMacc'][pos] + accession
+        seq['ELMType'][pos] = seq['ELMType'][pos] + vals[2]
+        seq['ELMIdentifier'][pos] = seq['ELMIdentifier'][pos] + vals[3]
+  return seq
+
+def readSIFTSparse(inSIFTSparse,ELMpos,seq):
+  '''Read 2nd struct from PDB following SIFTS parsing'''
+#  PDBss = OrderedDict()
+  PDBss = {}
+  PDBss['PDBid'] = list('-' * len(seq['res']))
+  PDBss['PDBchain'] = list('-' * len(seq['res']))
+  PDBss['PDBseq'] = list('-' * len(seq['res']))
+  PDBss['PDBss'] = list('-' * len(seq['res']))
+  PDBss['PDBseqres'] = list('-' * len(seq['res']))
+  PDBss['PDBseqatom'] = list('-' * len(seq['res']))
+  PDBss['PDBdis'] = list('-' * len(seq['res']))
+  fastaseqs = SeqIO.parse(open(inSIFTSparse),'fasta')
+  for fasta in fastaseqs:
+    if fasta.id[0:10] == ELMpos.keys()[0]:
+#      if 'sequence' in fasta.id:
+#        for pos in range(0,len(seq['res'])):
+#          if seq['ELMacc'][pos] == '-':
+#            continue
+#          if PDBss['PDBid'][pos] != '-':
+#            PDBss['PDBid'][pos] = '%s,%s' % (PDBss['PDBid'][pos],fasta.id[18:22])
+#            PDBss['PDBchain'][pos] = '%s,%s' % (PDBss['PDBchain'][pos],fasta.id[23])
+#          else:
+#            PDBss['PDBid'][pos] = fasta.id[18:22]
+#            PDBss['PDBchain'][pos] = fasta.id[23]
+#        PDBss['PDBid'][pos] = fasta.id[18:22]
+#        PDBss['PDBchain'][pos] = fasta.id[23]
+      if 'sequence' in fasta.id:
+        for pos in range(0,len(fasta.seq)):
+          if PDBss['PDBseq'][pos] != '-':
+            PDBss['PDBseq'][pos] = '%s,%s' % (PDBss['PDBseq'][pos],fasta.seq[pos])
+          else:
+            PDBss['PDBseq'][pos] = fasta.seq[pos]
+#          PDBss['PDBseq'][pos] = fasta.seq[pos]
+          if PDBss['PDBseq'][pos] != '-':
+            if PDBss['PDBid'][pos] != '-':
+              PDBss['PDBid'][pos] = '%s,%s' % (PDBss['PDBid'][pos],fasta.id[18:22])
+              PDBss['PDBchain'][pos] = '%s,%s' % (PDBss['PDBchain'][pos],fasta.id[23])
+            else:
+              PDBss['PDBid'][pos] = fasta.id[18:22]
+              PDBss['PDBchain'][pos] = fasta.id[23] 
+      elif 'secstr' in fasta.id:
+        for pos in range(0,len(fasta.seq)):
+          if PDBss['PDBss'][pos] != '-':
+            PDBss['PDBss'][pos] = '%s,%s' % (PDBss['PDBss'][pos],fasta.seq[pos])
+          else:
+            PDBss['PDBss'][pos] = fasta.seq[pos]
+#          PDBss['PDBss'][pos] = fasta.seq[pos]
+      elif 'disorder' in fasta.id:
+        for pos in range(0,len(fasta.seq)):
+          if PDBss['PDBdis'][pos] != '-':
+            PDBss['PDBdis'][pos] = '%s,%s' % (PDBss['PDBdis'][pos],fasta.seq[pos])
+          else:
+            PDBss['PDBdis'][pos] = fasta.seq[pos]
+#          PDBss['PDBdis'][pos] = fasta.seq[pos]
+#        break  # activate to read only first PDB for each ELMid
+      elif 'SEQRES' in fasta.id:
+        for pos in range(0,len(fasta.seq)):
+          if PDBss['PDBseqres'][pos] != '-':
+            PDBss['PDBseqres'][pos] = '%s,%s' % (PDBss['PDBseqres'][pos],fasta.seq[pos])
+          else:
+            PDBss['PDBseqres'][pos] = fasta.seq[pos]
+      elif 'SEQATOM' in fasta.id:
+        for pos in range(0,len(fasta.seq)):
+          if PDBss['PDBseqatom'][pos] != '-':
+            PDBss['PDBseqatom'][pos] = '%s,%s' % (PDBss['PDBseqatom'][pos],fasta.seq[pos])
+          else:
+            PDBss['PDBseqatom'][pos] = fasta.seq[pos]
+  return PDBss
+
+def printTable(results):
+  '''Print parsing results as table'''
+  for row in zip(*([key] + value for key, value in results.items())):
+    print '\t'.join(map(str, row))
+  return None
+
+# Make dict of input files
+seq = readFasta(infasta)
+infasta.close()
+
+parsedELM = readELMinstances(inELMinstances)
+inELMinstances.close()
+ELMpos = mapELMpositions(parsedELM,primaryAcc)
+seq = placeELM(seq,ELMpos)
+
+predictions = readJPred(injnet)
+injnet.close()
+
+PDBss = readSIFTSparse(inSIFTSparse,ELMpos,seq)
+#print type(PDBss)
+
+results = seq.copy()  # merge tables
+results.update(PDBss)
+results.update(predictions)
+
+printTable(results)
+
---
+++ ELMotiSt_PDBssSEQATOM.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+###############################################################################
+#
+# Generate parsed table of ELM structural information
+# File name:ELMotiSt_PDBssSEQATOM.sh
+# Author: Nicolas Palopoli
+# Date created: 2015/10/05
+# Date last modified: 2015/11/13
+# Bash version: 4.3.11(1)-release 
+#
+###############################################################################
+
+### FUNCTIONS ###
+
+# Test file/dir exists
+function testexist {
+  if [ ! -f $1 ] 
+  then
+    if [ ! -e $1 ]
+    then
+      echo "ERROR. Missing $1. Exit."
+      exit
+    fi    
+  fi
+}
+
+# Remove file if exists
+function testrm {
+  if [ -f $1 ]
+  then
+    rm "$1"
+  fi
+}
+
+# Print help
+function usage {
+  description="$(basename "$0")\nProgram to generate parsed table of ELM structural information.\nArguments:\n"
+  arguments="-h|--help\tShow this help\n
+             -e|--inelm\tPath to elm_instances.[date].tsv file\n
+             -j|--dirjpred\tPath to directory with JPred output files\n
+             -s|--insifts\tPath to parsed SIFTS file\n"
+  echo -e -n $description $arguments
+}
+
+### SETUP ###
+
+# Set default paths
+dirjpred='/home/npalopoli/20150924_ELM-Struct/JPred/elm_instances.fasta_ALL'
+inelm='/home/npalopoli/SLiMBench/ELMmap/elm_instances.2015-08-27.tsv'
+#insifts='/home/npalopoli/SIFTSParse/parseSIFTS_withELMID.out'
+insifts='/home/npalopoli/SIFTSParse/parseSIFTS_PDBssSEQATOM_withELMID.out'
+
+# Parse arguments to override defaults
+while [[ $# > 0 ]]  # number of positional parameters
+do
+  argument="$1"
+  case $argument in
+    -e|--inelm)
+      inelm="$2"
+      testexist $inelm
+      shift  # past argument
+      ;;
+    -j|--dirjpred)
+      dirjpred="$2"
+      testexist $dirjpred
+      shift
+      ;;
+    -s|--insifts)
+      insifts="$2"
+      testexist $insifts
+      shift
+      ;;
+    -h|--help)
+      usage
+      exit
+      ;;
+    *)
+      shift  # unknown option
+      ;;
+  esac
+  shift # past argument or value
+done
+
+# Check required files
+testexist ELMotiSt_PDBssSEQATOM.py
+
+# Write list of files to process
+if [ ! -f ./files.lst ]
+then
+  ls "$dirjpred"/*.fasta | xargs -n 1 basename | cut -d'.' -f 1 >files.lst
+# Sample subsets for testing
+# ls /home/npalopoli/20150924_ELM-Struct/JPred/elm_instances.fasta_dir_output/sp-P43489-TNR4_HUMAN.jnet | xargs -n 1 basename | cut -d'.' -f 1 >files.lst
+# ls $dir/*DROME.fasta | xargs -n 1 basename | cut -d'.' -f 1 >files.lst
+fi
+
+# Check/create output directory
+if [ ! -d ./ELMotiStELM_PDBssSEQATOM_output ]
+then
+  mkdir ./ELMotiStELM_PDBssSEQATOM_output 
+fi
+
+# Remove list of result files if existing
+testrm files_tsv.lst
+
+### START ###
+
+# Run ELMotiSt_PDBssSEQATOM.py for each in files.lst
+while read line
+do
+  accession=`head -1 "$dirjpred"/"$line".fasta | cut -d'|' -f 2`
+  ./ELMotiSt_PDBssSEQATOM.py "$dirjpred"/"$line".fasta "$dirjpred"/"$line".jnet "$inelm" "$insifts" "$accession" >ELMotiStELM_PDBssSEQATOM_output/"$line".tsv
+  echo "$line".tsv >>files_tsv.lst
+done <files.lst
+
+# Collect results in single output
+sample=`head -1 files.lst`
+head -1 ELMotiStELM_PDBssSEQATOM_output/"$sample".tsv>ELMotiStELM_PDBssSEQATOM_all.tsv
+while read line
+do
+  tail -n +2 ELMotiStELM_PDBssSEQATOM_output/"$line" >>ELMotiStELM_PDBssSEQATOM_all.tsv
+done<files_tsv.lst
+
+# Cleanup
+testrm files.lst
+testrm files_tsv.lst
